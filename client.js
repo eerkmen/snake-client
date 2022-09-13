7baf56a2ff6344d47b54bf53dcc7c5986d34d939
Efe Erkmen <efeerkmen@gmail.com>
2022-09-13 20:49:18 +0000

    first commit

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+const { IP, PORT } = require('./constants');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server!');
+    conn.write(`Name: EFE`);
+  });
+  
+  return conn;
+};
---
+++ play.js
@@ -0,0 +1,7 @@
+const net = require("net");
+
+// establishes a connection with the game server
+
+
+console.log("Connecting ...");
+connect();
