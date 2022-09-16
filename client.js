737014c4054710885800198d645fa97806f35fb0
eerkmen <efeerkmen@gmail.com>
2022-09-16 00:00:00 +0000

    create connect function at client.js

--- client.js
+++ client.js
@@ -1,3 +1,11 @@
 const net = require('net');
 const { IP, PORT } = require('./constants');
 
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+  });
+
+  
+};
