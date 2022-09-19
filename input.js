c5e6a974f93ea9880bae7d64ec5f6f93d480a05e
eerkmen <efeerkmen@gmail.com>
2022-09-19 00:00:00 +0000

    add stdin to setupinput function at input.js

--- input.js
+++ input.js
@@ -2,5 +2,11 @@ const { keys } = require('./constants');
 let connection;
 
 const setupInput = function(conn) {
- 
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
 };
